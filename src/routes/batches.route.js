0e093a9d616e590e10a8486f02d3078f7d98c00d
JuruLOSS <32490299+Ceiera@users.noreply.github.com>
2023-11-21 03:20:47 +0700

    user badges more prettier

--- src/routes/batches.route.js
+++ src/routes/batches.route.js
@@ -14,11 +14,11 @@ batchesRouter.patch("/:batchId", batchesController.updateBatchById);
 
 batchesRouter.delete("/:batchId", batchesController.deleteBatchById);
 
-batchesRouter.get("/:batchId/participants", batchesController.getAllParticipants);
+batchesRouter.get("/:batchId/participants", batchesController.getAllParticipantsById);
 
 batchesRouter.post("/:batchId/participants", batchesController.addParticipant);
 
-batchesRouter.get("/:batchId/mentors", batchesController.getAllMentors);
+batchesRouter.get("/:batchId/mentors", batchesController.getAllMentorsById);
 
 batchesRouter.post("/:batchId/mentors", batchesController.addMentor);
 
